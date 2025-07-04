4db87fd322d943dce6b5b1d08cc9d6f9b6e97f65
Den K. <d@komaroff.me>
2025-07-04 08:41:59 +0300

    entering point

--- src/__main__.py
+++ src/__main__.py
@@ -1,9 +1,6 @@
-from domain import LegalEntity
-# import uvicorn
-# from .route import app
+from services import init
 # from .argus import argus
 
 
 if __name__ == '__main__':
-    a = LegalEntity(tin='9726052044')
-    # uvicorn.run('route:app', host='0.0.0.0', port=9000)
+    init()
